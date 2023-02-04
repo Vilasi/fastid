422523bc9cba9965c7bf4708e72d2ba1b4bef3ba
Vilasi <VilasiCoding@gmail.com>
2023-02-04 04:22:38 -0500

    Added testing to remove repeated values from pool of generated IDs

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const idRegistry = [];
+let recursionChecker = 0;
+
 export default function idmon(num = 16) {
+  //Maximum Unique IDs Reached Check
+  if (recursionChecker > 10) {
+    return console.error(
+      'ERROR IDMon - Maximum Unique IDs reached. Increase ID size for more unique values.'
+    );
+  }
+
+  // Error Handling
   if (isNaN(num) || !num || num < 1) {
     return console.error(
       'idmon Argument must be number greater than or equal to 1'
@@ -15,7 +26,17 @@ export default function idmon(num = 16) {
     );
   }
 
-  return iterationsArray.join('');
+  const id = iterationsArray.join('');
+
+  // Check if ID has already been generated.
+  if (idRegistry.length > 0 && idRegistry.indexOf(id) !== -1) {
+    console.log(idRegistry);
+    recursionChecker++;
+    return idmon(num);
+  } else {
+    idRegistry.push(id);
+    return id;
+  }
 }
 
 function asciiPicker(min, max) {
@@ -37,4 +58,9 @@ function asciiPicker(min, max) {
 //Get random number within specified minimum constraint and maximum constraint.
 function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const array = [];
+for (let i = 0; i < 50; i++) {
+  array.push(idmon(1));
 }
